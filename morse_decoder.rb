18e34b9f1a93f9a25db511af0c5c53a07801cedb
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-07-20 12:25:01 +0100

    Fixing linters

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -7,18 +7,22 @@ class MorseDecoder
     "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
     "--.." => "Z"
   }.freeze
+
+  def self.decode_char(morse_char)
+    MORSE_CODE[morse_char] || " "
+  end
+
+  def self.decode_word(morse_word)
+    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+  end
+
+  def self.decode_message(morse_message)
+    morse_words = morse_message.split("   ")
+    decoded_words = morse_words.map { |morse_word| decode_word(morse_word) }
+    decoded_words.join(" ")
+  end
 end
 
-def self.decode_char(morse_char)
-  MORSE_CODE[morse_char] || " "
-end
-
-def self.decode_word(morse_word)
-  morse_word.split.map { |morse_char| decode_char(morse_char) }.join
-end
-
-def self.decode_message(morse_message)
-  morse_words = morse_message.split("   ")
-  decoded_words = morse_words.map { |morse_word| decode_word(morse_word) }
-  decoded_words.join(" ")
-end
+morse_message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+decoded_message = MorseDecoder.decode_message(morse_message)
+puts decoded_message
