90a6352ef3a47e8e081f20baf5ddbe7abf5d65f7
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-20 15:05:18 +0330

    Remove linter errors.

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,15 +1,15 @@
 class MorseDecoder
   MORSE_CODE = {
-    ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
-    "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
-    "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
-    ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-    "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-    "--.." => "Z"
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
   }.freeze
 
   def self.decode_char(morse_char)
-    MORSE_CODE[morse_char] || " "
+    MORSE_CODE[morse_char] || ' '
   end
 
   def self.decode_word(morse_word)
@@ -17,13 +17,13 @@ class MorseDecoder
   end
 
   def self.decode_message(morse_message)
-    morse_words = morse_message.split("   ")
+    morse_words = morse_message.split('   ')
     decoded_words = morse_words.map { |morse_word| decode_word(morse_word) }
-    decoded_words.join(" ")
+    decoded_words.join(' ')
   end
 end
 
 # Test with the provided Morse code message
-morse_message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+morse_message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_message = MorseDecoder.decode_message(morse_message)
 puts decoded_message
